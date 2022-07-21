ef01140d62c312f78f8f2118de0b0bef48b334c8
LiminLi-xjtu <59948715+LiminLi-xjtu@users.noreply.github.com>
2022-07-21 19:33:34 +0800

    Add files via upload

---
+++ demo1.R
@@ -0,0 +1,17 @@
+# use Dimensionality.R to reduce the dimensionality
+
+data <- R.matlab::readMat('C:\\Users\\pc\\Desktop\\sim\\sim1.mat')
+# data after dimensionality reduction
+X <- data$sim.DiffusionMap
+X <- data.frame(dim1=X[,1],dim2=X[,2])
+
+# calculate labels
+result<-mylabelling_fix(X)
+label<-result[["label"]]
+cutoff<-result[["cutoff"]]
+root<-result[["root"]]
+plot(X,col=label)
+
+# calculate pseudotime
+pseudotime = align(X,label,root,c(40,100,cutoff))
+ggplot(X, aes(x = dim1, y = dim2, color = pseudotime)) + geom_point() + scale_color_viridis_c(option = "viridis")
